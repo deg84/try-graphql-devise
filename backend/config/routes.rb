3aa899f3eb5b0c9fadacbf82c5b27d7c9c81d37c
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-08-26 08:57:17 +0000

    APIでユーザー登録、ログイン出来る

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-
-  mount_graphql_devise_for User, at: 'graphql_auth'
-  as :user do
-    # Define routes for User within this block.
-  end
+  mount_graphql_devise_for(User, {})
   post "/graphql", to: "graphql#execute"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
