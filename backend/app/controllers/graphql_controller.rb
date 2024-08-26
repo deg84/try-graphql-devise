b15d57ca670335625399eb8bde4ac60ebe994163
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-08-26 09:36:58 +0000

    GraphqlControllerにGraphqlDeviseの設定

--- backend/app/controllers/graphql_controller.rb
+++ backend/app/controllers/graphql_controller.rb
@@ -5,17 +5,15 @@ class GraphqlController < ApplicationController
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
+  include GraphqlDevise::SetUserByToken
 
   def execute
     variables = prepare_variables(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
-    context = {
-      # Query context goes here, for example:
-      # current_user: current_user,
-    }
+    context = gql_devise_context(User)
     result = BackendSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
-    render json: result
+    render json: result unless performed?
   rescue StandardError => e
     raise e unless Rails.env.development?
     handle_error_in_development(e)
