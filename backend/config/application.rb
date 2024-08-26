48d4e1ad367420bfa8bb145a4a7695b38a844eb2
DEGUCHI Tatsuya <t.deg84@gmail.com>
2024-08-26 06:45:12 +0000

    devise_token_authがrails 7.2にまだ対応していないのでrails 7.1.4にダウングレード

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -20,22 +20,13 @@ Bundler.require(*Rails.groups)
 
 module Backend
   class Application < Rails::Application
-    config.active_record.query_log_tags_enabled = true
-    config.active_record.query_log_tags = [
-      # Rails query log tags:
-      :application, :controller, :action, :job,
-      # GraphQL-Ruby query log tags:
-      current_graphql_operation: -> { GraphQL::Current.operation_name },
-      current_graphql_field: -> { GraphQL::Current.field&.path },
-      current_dataloader_source: -> { GraphQL::Current.dataloader_source_class },
-    ]
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_lib(ignore: %w[assets tasks])
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #
